eec916ce5f25e0001e085f28ff23532d34472cf1
Superkunair <vainillaweasel@gmail.com>
2020-04-20 22:19:42 -0500

    deleting some test files that i created

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,8 @@ import './index.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import * as serviceWorker from './serviceWorker';
 import NavBar from './components/Navigation/Navbar';
-import {TestSideMenu} from './components/Navigation/SideMenu';
-import {GridNavBar} from './layouts/Layout';
 //ReactDOM.render(<TestSideMenu />, document.getElementById('navigationMenu'));
-//ReactDOM.render(<NavBar/>,document.getElementById('navBar'))
-ReactDOM.render(<GridNavBar />, document.getElementById('root'));
+ReactDOM.render(<NavBar />, document.getElementById('navBar'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
